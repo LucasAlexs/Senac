b53ddcce35279aa20e101b27b470702d6500f29a
LucasAlexs <lucas.leal.santos@itec.ufpa.br>
2023-09-28 19:52:53 -0300

    Foi adicionado o Arquivo Desafio 3 do módulo 1

---
+++ desafio3.py
@@ -0,0 +1,15 @@
+#
+produto1 = "computador"
+produto2 = "TV"
+preco1 = 2100.5
+preco2 = 1830.0
+idade = 30
+codigo = 5291
+genero = "F"
+#
+#
+print(f"O produto {produto1} custa R$ {preco1}")
+print(f"O produto {produto2} custa R$ {preco2}")
+
+print(f"Código: {5291}")
+print(f"Dados da pessoa:\ngênero  {genero} e idade {idade}")
